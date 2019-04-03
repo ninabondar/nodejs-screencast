3534cc588bd80d75cdd86a177aa904faa0d15185
Nina Bondar <nina.bondar.cs@gmail.com>
2019-04-03 09:45:17 +0300

    minor change

--- server.js
+++ server.js
@@ -2,15 +2,22 @@ const http = require("http");
 const url = require("url");
 
 const server = new http.Server(); // == EventEmitter
-server.listen(8000);
 
+const validURLs = ["index.html", "contact.html"];
 
 const handleReq = (req, res) => {
-  console.log(req.method, req.url);
-
   let parsedUrl = url.parse(req.url, true);
-  console.log(parsedUrl, "parsed", parsedUrl.query);
-  res.end(parsedUrl.query.message);
+  if (validURLs.includes(parsedUrl.pathname.slice(1))) {
+    console.log("=====SUCCCEEEEESS======", parsedUrl.pathname);
+    res.statusCode = 200;
+    res.end(JSON.stringify(parsedUrl.query));
+  } else {
+    res.statusCode = 404;
+    res.end("sorry, no such page found");
+  }
 };
 
 server.on("request", (req, res) => handleReq(req, res));
+
+
+server.listen(8000);

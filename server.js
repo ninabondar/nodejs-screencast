9f105931ab2f1c48fbe5951535c8c1a9ebb8deb0
Nina Bondar <nina.bondar.cs@gmail.com>
2019-04-03 14:23:30 +0300

    add basic server to open index.html

--- server.js
+++ server.js
@@ -1,23 +1,41 @@
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
 
 const server = new http.Server(); // == EventEmitter
+const port = 8000;
+server.on("request", (req, res) => handleReq(req, res));
+server.listen(port, () => {
+  console.log("Listening at http://localhost:" + port);
+});
 
-const validURLs = ["index.html", "contact.html"];
-
-const handleReq = (req, res) => {
-  let parsedUrl = url.parse(req.url, true);
-  if (validURLs.includes(parsedUrl.pathname.slice(1))) {
-    console.log("=====SUCCCEEEEESS======", parsedUrl.pathname);
-    res.statusCode = 200;
-    res.end(JSON.stringify(parsedUrl.query));
-  } else {
-    res.statusCode = 404;
-    res.end("sorry, no such page found");
-  }
+const sendRequest = (res, data) => {
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+    "Content-Length": data.length
+  });
+  res.write(data);
+  res.end();
 };
 
-server.on("request", (req, res) => handleReq(req, res));
+const handleReq = (req, res) => {
+  let parsedUrl = url.parse(req.url);
 
-
-server.listen(8000);
+  if (parsedUrl.pathname === "/") {
+    fs.readdir(__dirname, (err, files) => {
+      if (err) {
+        console.error("===file error===");
+      } else if (files.includes("index.html")) {
+        fs.readFile("index.html", "utf-8", (error, file) => {
+          if (error) {
+            console.log(error, "===file error===");
+          }
+          sendRequest(res, file);
+        });
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.end("something went wrong");
+  }
+};
